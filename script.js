02c1993f714c9ac4ced5c08d51120d88ad514ccb
yasasiku <ayu.2223@icloud.com>
2026-08-16 21:40:38 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function playSound() {
+    const sound = document.getElementById("sound");
+    sound.currentTime = 0;
+    sound.play();
+}
+
+function playSound2() {
+    const sound2 = document.getElementById("sound2");
+    sound2.currentTime = 0;
+    sound2.play();
+}
+
+function playSound3() {
+    const sound3 = document.getElementById("sound3");
+    sound3.currentTime = 0;
+    sound3.play();
+}
+function playSound4() {
+    const sound4 = document.getElementById("sound4");
+    sound4.currentTime = 0;
+    sound4.play();
+}
+
+function playSound5() {
+    const sound5 = document.getElementById("sound5");
+    sound5.currentTime = 0;
+    sound5.play();
+}
+
+function changeImage4() {
+    const img = document.getElementById("photo-4");
+   
+    img.src = "images/27.png";
+}
